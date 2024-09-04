2add3f662e7faa7fbdbad55dac284c1aa37306e6
Jhonnatan Gomez <jalfgomez@poligran.edu.co>
2024-09-03 20:43:24 -0500

    Se crea el repositorio para resolver ejercicio extraer año del numero de la tarjeta de identidad

---
+++ src/Ejercicios/ExtractorAño.java
@@ -0,0 +1,14 @@
+package Ejercicios;
+
+public class ExtractorAño {
+	
+	public static void main(String[] args) {
+				
+		int b = 2;
+		int c = 3;
+		
+		System.out.println("la suma es: " + (b + c)); 
+		
+	}
+
+}
